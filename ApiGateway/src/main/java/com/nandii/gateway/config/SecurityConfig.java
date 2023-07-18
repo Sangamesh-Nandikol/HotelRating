e30e4dfb2d31e1808d6bab32d61f7cee32b9403f
sangamesh <nandikolsangamesh27@gmail.com>
2023-07-18 14:43:24 +0530

    Implementing okta on api gateway service

---
+++ ApiGateway/src/main/java/com/nandii/gateway/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.nandii.gateway.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig  {
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+		
+		httpSecurity
+		 	.authorizeExchange()
+		 	.anyExchange()
+		 	.authenticated()
+		 	.and()
+		 	.oauth2Client()
+		 	.and()
+		 	.oauth2ResourceServer()
+		 	.jwt();
+		 	
+		
+		return httpSecurity.build();
+		
+	}
+}
---
+++ ApiGateway/src/main/java/com/nandii/gateway/responsebean/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.nandii.gateway.responsebean;
+
+import java.util.Collection;
+
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+
+	private String userId;
+	private String 	accessToken;
+	private String refreshToken;
+	private long expireAt;
+	private Collection<String> authorities; 
+}
